2efcffe59237e7efc46e6050befe010758ce754a
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2023-07-30 12:36:15 -0400

    Fixed the npm install so it should be faster

--- pipeline/pm2_stop_handling_app.sh
+++ pipeline/pm2_stop_handling_app.sh
@@ -4,6 +4,5 @@ cd /home/ubuntu/blackedge-api
 CWD=$(pwd)
 echo "Using PM2 to start the Blackedge API server from ${CWD}"
 ls -l
-apt install -y npm
 source .venv/bin/activate
 pm2 stop "python3 -m flask run"
--- pipeline/setup_dependencies.sh
+++ pipeline/setup_dependencies.sh
@@ -5,6 +5,7 @@ cd /home/ubuntu/blackedge-api
 echo "Updating system"
 apt-get update
 apt install -y python3.10-venv
+apt install -y npm
 
 echo "Setting Venv"
 python3 -m venv .venv
@@ -13,7 +14,7 @@ source .venv/bin/activate
 echo "Installing dependencies"
 pip install Flask
 pip install -r requirements.txt
-npm install pm2@latest -g 
+npm install pm2 -g 
 
 echo "Ensuring log location"
 mkdir -p /var/log/pm2/blackedge-api
