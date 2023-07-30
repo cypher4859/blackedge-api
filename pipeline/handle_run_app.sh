c1a6ff4bd84558a6a3cc79d532c7b0b4b52bef79
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2023-07-29 21:04:35 -0400

    fixed the venv issue

--- pipeline/handle_run_app.sh
+++ pipeline/handle_run_app.sh
@@ -3,5 +3,6 @@
 cd /home/ubuntu/blackedge-api
 CWD=$(pwd)
 echo "Running the Blackedge API server from ${CWD}"
+ls -l
 source .venv/bin/activate
 python3 -m flask run
--- pipeline/setup_dependencies.sh
+++ pipeline/setup_dependencies.sh
@@ -4,7 +4,8 @@ echo "Installing requirements"
 cd /home/ubuntu/blackedge-api
 
 apt-get update
-apt-get upgrade
+apt-get -y upgrade
+apt install -y python3.10-venv
 python3 -m venv .venv
 source .venv/bin/activate
 pip install Flask
