7bab430cd7ff978c256e6a878216fb0c6ba7014a
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2023-07-30 00:13:31 -0400

    surely doing this will work

--- pipeline/pm2_stop_handling_app.sh
+++ pipeline/pm2_stop_handling_app.sh
@@ -5,6 +5,5 @@ CWD=$(pwd)
 echo "Using PM2 to start the Blackedge API server from ${CWD}"
 ls -l
 apt install -y npm
-npm install pm2@latest -g
 source .venv/bin/activate
-pm2 stop "python3 -m flask run"
+npm install pm2@latest -g && pm2 stop "python3 -m flask run"
