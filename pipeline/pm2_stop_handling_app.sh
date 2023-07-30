7e65eddbfde462ac347f54e9392094c827c662f9
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2023-07-30 00:11:17 -0400

    Not sure why this script isn'tinstalling pm2

--- pipeline/pm2_stop_handling_app.sh
+++ pipeline/pm2_stop_handling_app.sh
@@ -4,7 +4,7 @@ cd /home/ubuntu/blackedge-api
 CWD=$(pwd)
 echo "Using PM2 to start the Blackedge API server from ${CWD}"
 ls -l
-source .venv/bin/activate
 apt install -y npm
 npm install pm2@latest -g
+source .venv/bin/activate
 pm2 stop "python3 -m flask run"
